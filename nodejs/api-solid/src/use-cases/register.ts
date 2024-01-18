73ebde384bdd9b637c3bf7263fd05a1b484d4e09
Paulo Henrique F. de Souza <phferreirasouza106@gmail.com>
2024-01-18 07:49:04 -0300

    feat(api-solid): repository pattern

--- nodejs/api-solid/src/use-cases/register.ts
+++ nodejs/api-solid/src/use-cases/register.ts
@@ -1,4 +1,5 @@
 import { prisma } from "@/lib/prisma";
+import { PrismaUsersRepository } from "@/repositories/prisma-users-repository";
 import { hash } from "bcryptjs";
 
 interface RegisterUseCaseRequest {
@@ -19,16 +20,16 @@ export async function registerUseCase({
 	});
 
 	if (userWithSameEmail) {
-		throw new Error("E-mail already exists!");
+		throw new Error("E-mail already exists.");
 	}
 
 	const password_hash = await hash(password, 6);
 
-	await prisma.user.create({
-		data: {
-			name,
-			email,
-			password_hash,
-		},
+	const prismaUsersRepository = new PrismaUsersRepository();
+
+	await prismaUsersRepository.create({
+		name,
+		email,
+		password_hash,
 	});
 }
