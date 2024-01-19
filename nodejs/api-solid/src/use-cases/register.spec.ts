88cc3f8b5d98cea46483bf26c4cfa9513d236eff
Paulo Henrique F. de Souza <phferreirasouza106@gmail.com>
2024-01-18 21:39:17 -0300

    feat(api-solid): first unit test

--- nodejs/api-solid/src/use-cases/register.spec.ts
+++ nodejs/api-solid/src/use-cases/register.spec.ts
@@ -1,5 +1,24 @@
-import { expect, test } from "vitest";
+import { compare } from "bcryptjs";
+import { describe, expect, it } from "vitest";
+import { RegisterUseCase } from "./register";
 
-test("check if it works", () => {
-	expect(2 + 2).toBe(4);
+describe("Register Use Case", () => {
+	it("should hash user password upon registration", async () => {
+		const registerUseCase = new RegisterUseCase({});
+
+		const password = "123456";
+
+		const { user } = await registerUseCase.execute({
+			name: "John Doe",
+			email: "johndoe@example.com",
+			password,
+		});
+
+		const isPasswordCorrectlyHashed = await compare(
+			password,
+			user.password_hash,
+		);
+
+		expect(isPasswordCorrectlyHashed).toBe(true);
+	});
 });
