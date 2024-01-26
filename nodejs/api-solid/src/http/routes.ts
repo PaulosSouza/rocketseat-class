0a049659390e00e859bbf14f799720ccb453ad37
Paulo Henrique F. de Souza <phferreirasouza106@gmail.com>
2024-01-26 19:41:08 -0300

    feat(api-solid): base profile route

--- nodejs/api-solid/src/http/routes.ts
+++ nodejs/api-solid/src/http/routes.ts
@@ -1,9 +1,13 @@
 import { FastifyInstance } from "fastify";
 
 import { authenticate } from "./controllers/authenticate";
+import { profile } from "./controllers/profile";
 import { register } from "./controllers/register";
 
 export async function appRoutes(app: FastifyInstance) {
 	app.post("/users", register);
 	app.post("/authenticate", authenticate);
+
+	/** Authenticated routes */
+	app.get("/me", profile);
 }
