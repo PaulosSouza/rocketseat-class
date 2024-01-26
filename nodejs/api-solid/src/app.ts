d3028d7a698d988d39bba4f1b2208b7899369198
Paulo Henrique F. de Souza <phferreirasouza106@gmail.com>
2024-01-26 20:05:11 -0300

    feat(api-solid): fastify jwt implementations

--- nodejs/api-solid/src/app.ts
+++ nodejs/api-solid/src/app.ts
@@ -1,11 +1,15 @@
-import console from "console";
+import fastifyJwt from "@fastify/jwt";
 import fastify from "fastify";
 import { ZodError } from "zod";
 import { fromZodError } from "zod-validation-error";
 import { env } from "./env";
 import { appRoutes } from "./http/routes";
 
-export const app = fastify({});
+export const app = fastify();
+
+app.register(fastifyJwt, {
+	secret: env.JWT_SECRET,
+});
 
 app.register(appRoutes);
 
