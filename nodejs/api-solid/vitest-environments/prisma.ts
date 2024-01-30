aa5d19e658dfb95b629aedce5529ab0e4d607e3a
Paulo Henrique F. de Souza <phferreirasouza106@gmail.com>
2024-01-29 21:55:17 -0300

    feat(api-solid): prisma test environment

--- nodejs/api-solid/vitest-environments/prisma.ts
+++ nodejs/api-solid/vitest-environments/prisma.ts
@@ -1,13 +1,41 @@
+import { execSync } from "node:child_process";
+import { randomUUID } from "node:crypto";
+import { env } from "@/env";
+import { PrismaClient } from "@prisma/client";
 import { Environment } from "vitest";
+
+const prisma = new PrismaClient();
+
+function generateDatabaseURL(schema: string) {
+	if (!env.DATABASE_URL) {
+		throw new Error("Please provide a DATABASE_URL environment variable.");
+	}
+
+	const url = new URL(env.DATABASE_URL);
+	url.searchParams.set("schema", schema);
+
+	return url.toString();
+}
 
 export default {
 	name: "prisma",
 	transformMode: "ssr",
 	async setup() {
-		console.log("Executou");
+		const schema = randomUUID();
+		const databaseURL = generateDatabaseURL(schema);
+
+		process.env.DATABASE_URL = databaseURL;
+
+		execSync("pnpm prisma migrate deploy");
 
 		return {
-			teardown(global) {},
+			async teardown() {
+				await prisma.$executeRawUnsafe(
+					`DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+				);
+
+				await prisma.$disconnect();
+			},
 		};
 	},
 } as Environment;
