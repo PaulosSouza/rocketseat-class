f71942c3136fb0d478b6be766ea58e7920041243
Paulo Henrique F. de Souza <phferreirasouza106@gmail.com>
2024-02-22 08:34:51 -0300

    feat(clean-ddd): abstracting the creation of entities

--- nodejs/clean-ddd/src/domain/use-cases/answer-question.ts
+++ nodejs/clean-ddd/src/domain/use-cases/answer-question.ts
@@ -1,32 +1,33 @@
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { Answer } from "../entities/answer";
 import { AnswersRepository } from "../repositories/answers-repository";
 
 interface AnswerQuestionUseCaseRequest {
-	instructorId: string;
-	questionId: string;
-	content: string;
+  instructorId: string;
+  questionId: string;
+  content: string;
 }
 
 interface AnswerQuestionUseCaseResponse {
-	answer: Answer;
+  answer: Answer;
 }
 
 export class AnswerQuestionUseCase {
-	constructor(private answerRepository: AnswersRepository) {}
+  constructor(private answerRepository: AnswersRepository) {}
 
-	async execute({
-		questionId,
-		instructorId,
-		content,
-	}: AnswerQuestionUseCaseRequest): Promise<AnswerQuestionUseCaseResponse> {
-		const answer = new Answer({
-			content,
-			authorId: instructorId,
-			questionId,
-		});
+  async execute({
+    questionId,
+    instructorId,
+    content,
+  }: AnswerQuestionUseCaseRequest): Promise<AnswerQuestionUseCaseResponse> {
+    const answer = Answer.create({
+      content,
+      authorId: new UniqueEntityID(instructorId),
+      questionId: new UniqueEntityID(questionId),
+    });
 
-		await this.answerRepository.create(answer);
+    await this.answerRepository.create(answer);
 
-		return { answer };
-	}
+    return { answer };
+  }
 }
