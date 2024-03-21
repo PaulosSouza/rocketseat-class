270c11f84b4626b5bd5c0ead9db4020ee1bd3bc9
Paulo Henrique <phferreirasouza106@gmail.com>
2024-03-21 15:05:16 -0300

    feat(clean-ddd): fetch recent questions use case

---
+++ nodejs/clean-ddd/src/domain/forum/application/use-cases/fetch-recent-questions.spec.ts
@@ -0,0 +1,44 @@
+import { InMemoryQuestionsRepository } from "test/repositories/in-memory-questions-repository";
+import { makeQuestion } from "test/factories/make-question";
+import { Slug } from "../../enterprise/entities/value-objects/slug";
+import { FetchRecentQuestionsUseCase } from "./fetch-recent-questions";
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
+let sut: FetchRecentQuestionsUseCase;
+
+describe("Fetch Recent Questions", () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository();
+    sut = new FetchRecentQuestionsUseCase(inMemoryQuestionsRepository);
+  });
+
+  it("should be able to fetch recent questions", async () => {
+    const randomDays = [20, 18, 23];
+
+    for (const day of randomDays) {
+      await inMemoryQuestionsRepository.create(makeQuestion({ createdAt: new Date(2022, 0, day) }));
+    }
+
+    const { questions } = await sut.execute({
+      page: 1,
+    });
+
+    expect(questions).toEqual([
+      expect.objectContaining({ createdAt: new Date(2022, 0, 23) }),
+      expect.objectContaining({ createdAt: new Date(2022, 0, 20) }),
+      expect.objectContaining({ createdAt: new Date(2022, 0, 18) }),
+    ]);
+  });
+
+  it("should be able to fetch paginated recent questions", async () => {
+    for (let index = 1; index <= 22; index++) {
+      await inMemoryQuestionsRepository.create(makeQuestion());
+    }
+
+    const { questions } = await sut.execute({
+      page: 2,
+    });
+
+    expect(questions).toHaveLength(2);
+  });
+});
