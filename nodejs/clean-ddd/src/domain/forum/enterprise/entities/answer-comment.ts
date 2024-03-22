ebe9c04bee33c8ff07a3ef26cfb4d06ea86bfd97
Paulo Henrique F. de Souza <phferreirasouza106@gmail.com>
2024-03-22 20:17:16 -0300

    feat(clean-ddd): comment base class

--- nodejs/clean-ddd/src/domain/forum/enterprise/entities/answer-comment.ts
+++ nodejs/clean-ddd/src/domain/forum/enterprise/entities/answer-comment.ts
@@ -1,39 +1,15 @@
 import { Entity } from "@/core/entities/entity";
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { Optional } from "@/core/types/optional";
+import { CommentProps } from "./comment";
 
-export interface AnswerCommentProps {
-  authorId: UniqueEntityID;
+export interface AnswerCommentProps extends CommentProps {
   answerId: UniqueEntityID;
-  content: string;
-  createdAt: Date;
-  updatedAt?: Date;
 }
 
 export class AnswerComment extends Entity<AnswerCommentProps> {
-  get content() {
-    return this.props.content;
-  }
-
-  get authorId() {
-    return this.props.authorId;
-  }
-
-  get createdAt() {
-    return this.props.createdAt;
-  }
-
-  get updatedAt() {
-    return this.props.updatedAt;
-  }
-
-  set content(content: string) {
-    this.props.content = content;
-    this.touch();
-  }
-
-  private touch() {
-    this.props.updatedAt = new Date();
+  get answerId() {
+    return this.props.answerId;
   }
 
   static create(props: Optional<AnswerCommentProps, "createdAt">, id?: UniqueEntityID) {
