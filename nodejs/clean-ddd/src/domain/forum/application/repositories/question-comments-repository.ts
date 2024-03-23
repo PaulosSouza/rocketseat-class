c3bd0222ce3351801e19bcd04af46b2226bde19a
Paulo Henrique F. de Souza <phferreirasouza106@gmail.com>
2024-03-22 21:17:56 -0300

    feat(clean-ddd): delete question comment use case

--- nodejs/clean-ddd/src/domain/forum/application/repositories/question-comments-repository.ts
+++ nodejs/clean-ddd/src/domain/forum/application/repositories/question-comments-repository.ts
@@ -2,4 +2,6 @@ import { QuestionComment } from "../../enterprise/entities/question-comment";
 
 export interface QuestionsCommentRepository {
   create(question: QuestionComment): Promise<void>;
+  findById(id: string): Promise<QuestionComment | null>;
+  delete(question: QuestionComment): Promise<void>;
 }
