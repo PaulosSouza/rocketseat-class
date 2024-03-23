2b37d82ba44ab9b9fd98266596a22ea14ccbdaea
Paulo Henrique F. de Souza <phferreirasouza106@gmail.com>
2024-03-22 21:24:05 -0300

    feat(clean-ddd): delete answer comment use case

--- nodejs/clean-ddd/src/domain/forum/application/repositories/answer-comments-repository.ts
+++ nodejs/clean-ddd/src/domain/forum/application/repositories/answer-comments-repository.ts
@@ -1,5 +1,7 @@
 import { AnswerComment } from "../../enterprise/entities/answer-comment";
 
-export interface AnswersCommentRepository {
-  create(answer: AnswerComment): Promise<void>;
+export interface AnswerCommentRepository {
+  findById(id: string): Promise<AnswerComment | null>;
+  create(answerComment: AnswerComment): Promise<void>;
+  delete(answerComment: AnswerComment): Promise<void>;
 }
--- nodejs/clean-ddd/test/repositories/in-memory-answer-comments-repository.ts
+++ nodejs/clean-ddd/test/repositories/in-memory-answer-comments-repository.ts
@@ -1,10 +1,26 @@
-import { AnswersCommentRepository } from "@/domain/forum/application/repositories/answer-comments-repository";
+import { AnswerCommentRepository } from "@/domain/forum/application/repositories/answer-comments-repository";
 import { AnswerComment } from "@/domain/forum/enterprise/entities/answer-comment";
 
-export class InMemoryAnswerCommentsRepository implements AnswersCommentRepository {
+export class InMemoryAnswerCommentsRepository implements AnswerCommentRepository {
   public items: AnswerComment[] = [];
 
   async create(answerComment: AnswerComment): Promise<void> {
     this.items.push(answerComment);
+  }
+
+  async delete(answerComment: AnswerComment) {
+    const itemIndex = this.items.findIndex((item) => item.id === answerComment.id);
+
+    this.items.splice(itemIndex, 1);
+  }
+
+  async findById(id: string) {
+    const answerComment = this.items.find((item) => item.id.toString() === id);
+
+    if (!answerComment) {
+      return null;
+    }
+
+    return answerComment;
   }
 }
