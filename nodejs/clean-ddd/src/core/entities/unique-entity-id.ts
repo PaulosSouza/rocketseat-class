0e25d2bfb799b5a249ad1bb6c646cbe5453f4112
Paulo Henrique F. de Souza <phferreirasouza106@gmail.com>
2024-03-28 07:03:46 -0300

    feat(clean-ddd): domain events structure

--- nodejs/clean-ddd/src/core/entities/unique-entity-id.ts
+++ nodejs/clean-ddd/src/core/entities/unique-entity-id.ts
@@ -1,17 +1,21 @@
 import { randomUUID } from "crypto";
 
 export class UniqueEntityID {
-	private value: string;
+  private value: string;
 
-	constructor(value?: string) {
-		this.value = value ?? randomUUID();
-	}
+  toString() {
+    return this.value;
+  }
 
-	toString() {
-		return this.value;
-	}
+  toValue() {
+    return this.value;
+  }
 
-	toValue() {
-		return this.value;
-	}
+  constructor(value?: string) {
+    this.value = value ?? randomUUID();
+  }
+
+  equals(id: UniqueEntityID) {
+    return id.toValue() === this.value;
+  }
 }
