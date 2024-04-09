64e423ad336887243a2271678e5d0e10d0ec0cc7
Paulo Henrique F. de Souza <phferreirasouza106@gmail.com>
2024-04-09 20:46:29 -0300

    feat(nest-ddd): refactoring authentication controller

--- nodejs/nest-ddd/src/infra/http/controllers/authenticate.controller.ts
+++ nodejs/nest-ddd/src/infra/http/controllers/authenticate.controller.ts
@@ -1,8 +1,5 @@
-import { PrismaService } from "@/infra/database/prisma/prisma.service";
-import { Body, Controller, HttpCode, Post, UnauthorizedException, UsePipes } from "@nestjs/common";
-import { JwtService } from "@nestjs/jwt";
-import { compare } from "bcryptjs";
-
+import { AuthenticateStudentUseCase } from "@/domain/forum/application/use-cases/authenticate-student";
+import { Body, Controller, HttpCode, Post, UsePipes } from "@nestjs/common";
 import { z } from "zod";
 import { ZodValidationPipe } from "../pipes/zod-validation.pipe";
 
@@ -15,10 +12,7 @@ type AuthenticateBodySchema = z.infer<typeof authenticateBodySchema>;
 
 @Controller("/sessions")
 export class AuthenticateController {
-  constructor(
-    private readonly prisma: PrismaService,
-    private readonly jwtService: JwtService,
-  ) {}
+  constructor(private readonly authenticateStudent: AuthenticateStudentUseCase) {}
 
   @Post()
   @HttpCode(201)
@@ -26,23 +20,16 @@ export class AuthenticateController {
   async handle(@Body() body: AuthenticateBodySchema) {
     const { email, password } = body;
 
-    const user = await this.prisma.user.findUnique({
-      where: {
-        email,
-      },
+    const result = await this.authenticateStudent.execute({
+      email,
+      password,
     });
 
-    if (!user) {
-      throw new UnauthorizedException("User credentials do not match.");
+    if (result.isLeft()) {
+      throw new Error();
     }
 
-    const isPasswordValid = await compare(password, user.password);
-
-    if (!isPasswordValid) {
-      throw new UnauthorizedException("User credentials do not match.");
-    }
-
-    const accessToken = this.jwtService.sign({ sub: user.id });
+    const { accessToken } = result.value;
 
     return { access_token: accessToken };
   }
