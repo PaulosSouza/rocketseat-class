a78a1d54f03f7bb303302f9b7f67984572ab8430
Paulo Henrique F. de Souza <phferreirasouza106@gmail.com>
2024-04-12 20:28:22 -0300

    feat(nest-ddd): implementing repositories

--- nodejs/nest-ddd/src/domain/forum/application/repositories/question-comments-repository.ts
+++ nodejs/nest-ddd/src/domain/forum/application/repositories/question-comments-repository.ts
@@ -1,9 +1,12 @@
 import { PaginationParams } from "@/core/repositories/pagination-params";
 import { QuestionComment } from "../../enterprise/entities/question-comment";
 
-export interface QuestionCommentsRepository {
-  create(question: QuestionComment): Promise<void>;
-  findManyByQuestionId(questionId: string, params: PaginationParams): Promise<QuestionComment[]>;
-  findById(id: string): Promise<QuestionComment | null>;
-  delete(question: QuestionComment): Promise<void>;
+export abstract class QuestionCommentsRepository {
+  abstract findManyByQuestionId(
+    questionId: string,
+    params: PaginationParams,
+  ): Promise<QuestionComment[]>;
+  abstract findById(id: string): Promise<QuestionComment | null>;
+  abstract create(questionComment: QuestionComment): Promise<void>;
+  abstract delete(questionComment: QuestionComment): Promise<void>;
 }
