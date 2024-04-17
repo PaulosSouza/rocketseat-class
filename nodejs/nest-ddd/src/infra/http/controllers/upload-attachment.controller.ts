2b016456ad3b794cef4df0c9969e4290ae003912
Paulo Henrique F. de Souza <phferreirasouza106@gmail.com>
2024-04-16 21:43:44 -0300

    fest(nest-ddd): upload attachment controller

---
+++ nodejs/nest-ddd/src/infra/http/controllers/upload-attachment.controller.ts
@@ -0,0 +1,33 @@
+import {
+  Controller,
+  FileTypeValidator,
+  HttpCode,
+  MaxFileSizeValidator,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from "@nestjs/common";
+import { FileInterceptor } from "@nestjs/platform-express";
+
+@Controller("/attachments")
+export class UploadAttachmentController {
+  //  constructor(private readonly getQuestionBySlug: GetQuestionBySlugUseCase) {}
+
+  @Post()
+  @HttpCode(201)
+  @UseInterceptors(FileInterceptor("file"))
+  async handle(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 1024 * 1024 * 2 }),
+          new FileTypeValidator({ fileType: ".(png|jpg|jpeg|pdf)" }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    console.log(file);
+  }
+}
