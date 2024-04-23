09757a64a9275ec91679b7c80758a4d79a75f325
Paulo Henrique <phferreirasouza106@gmail.com>
2024-04-23 15:11:05 -0300

    feat(nest-ddd): cache with redis

--- nodejs/nest-ddd/src/infra/main.ts
+++ nodejs/nest-ddd/src/infra/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from "./app.module";
 import { EnvService } from "./env/env.service";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { logger: false });
+  const app = await NestFactory.create(AppModule);
 
   const envService = app.get(EnvService);
   const port = envService.get("PORT");
